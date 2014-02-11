b4d2ff1e436de8e7270815aa1942902d9103c263
Jackson Jessup <jessupjn@umich.edu>
2014-02-10 23:23:53 -0500

    created basic files that will be needed...
    all are empty for now

---
+++ Windows_Application/CPRemoteApp/CPRemoteApp/Bluetooth Connections/BluetoothController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRemoteApp.Bluetooth_Connections
+{
+    class BluetoothController
+    {
+    }
+}
