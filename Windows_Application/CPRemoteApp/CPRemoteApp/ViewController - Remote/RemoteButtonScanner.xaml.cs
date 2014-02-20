39cfbd82ce326abc1262b5f69185fe0e9d9eb756
lukedgr <lukedgr@umich.edu>
2014-02-20 16:11:08 -0500

    starting the Button Scanner

---
+++ Windows_Application/CPRemoteApp/CPRemoteApp/ViewController - Remote/RemoteButtonScanner.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CPRemoteApp.ViewController___Remote
+{
+    public sealed partial class RemoteButtonScanner : UserControl
+    {
+        // -------------------------------------
+        private List<Button> button_list;
+        private int cur_index;
+
+        // -------------------------------------
+
+        public RemoteButtonScanner()
+        {
+            this.InitializeComponent();
+        }
+
+        //Should take in an input stream that it can read from to initialize all the buttons associated with it
+        public void initialize(Windows.Storage.StorageFile file)
+        {
+            
+        }
+    }
+}
