2db77ebd685d02c9e4a389aab90ae42bf3fcd18a
adnant <adnant@gmail.com>
2014-02-25 00:12:21 -0500

    Updated code to parse the data. data must be in the form "protocol.ircode/"

--- Hardware Stuff/Arduino/sample_read_write/sample_read_write.ino
+++ Hardware Stuff/Arduino/sample_read_write/sample_read_write.ino
@@ -1,7 +1,8 @@
 #include <SoftwareSerial.h>  
 
-int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
-int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
+int bluetoothTx =5 ;  // TX-O pin of bluetooth mate, Arduino D2
+int bluetoothRx = 4;  // RX-I pin of bluetooth mate, Arduino D3
+
 
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
@@ -11,13 +12,47 @@ void setup()
   bluetooth.begin(57600);  // Start bluetooth serial at 9600
 }
 
+
 void loop()
 {
   if(bluetooth.available())  // If the bluetooth sent any characters
   {
     // Send any characters the bluetooth prints to the serial monitor
-    Serial.print((char)bluetooth.read());  
+    char recvddata[80];
+    memset(recvddata, 0, 80); 
+    String b_protocol=""; 
+    String b_code=""; 
+    char endl='/'; 
+    int len= bluetooth.readBytesUntil(endl, recvddata, 80);
+    recvddata[len]='\0'; 
+    parseString(recvddata, b_protocol, b_code); 
+    Serial.println("Protocol: "+b_protocol); 
+    Serial.println("code: "+b_code); 
+    
   }
+  
+  sendBluetoothMessage(); 
+ 
+}
+
+
+void parseString (String message, String &b_protocol, String &b_code)
+{
+  int commaPosition; 
+  commaPosition = message.indexOf('.');
+  if(commaPosition != -1)
+  {
+    b_protocol = message.substring(0,commaPosition); 
+    b_protocol[commaPosition+1]='\0'; 
+    b_code = message.substring(commaPosition+1); 
+  }
+     
+ 
+}
+
+
+void sendBluetoothMessage () {
+  
   if(Serial.available())  // If stuff was typed in the serial monitor
   {
    char serialdata[80];
@@ -25,11 +60,11 @@ void loop()
    int lf=10; 
    
     // Send any characters the Serial monitor prints to the bluetooth
-   int len= Serial.readBytesUntil(lf, serialdata, 80);;
+   int len= Serial.readBytesUntil(lf, serialdata, 80);
     sendMessage(serialdata, len ); 
-  }
-  // and loop forever and ever!
+  }  
 }
+
 
 
 //outbound
@@ -44,3 +79,4 @@ void sendMessage(char* message, int len)
 		bluetooth.print(message);
 	}
 }
+
